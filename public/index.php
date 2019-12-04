967a81670f3eb54edfeedbcc6894ad47fd6d753e
亀井亜佐夫 <asao.kamei@gmail.com>
2019-12-04 16:50:09 +0900

    fixed links to example pages.

--- public/index.php
+++ public/index.php
@@ -9,8 +9,15 @@ $page->extend(__DIR__ . '/layouts/layout.php');
 ?>
 <h1>Welcome to PlainPages!</h1>
 <p>for raw php with low maintenance web sites. </p>
+
+<h2>Nested Template</h2>
+<ul>
+    <li><a href="sided.php">page with nested templates.</a></li>
+    <li><a href="sided2.php">page with nested templates but without explicitly starting a section.</a></li>
+</ul>
+
+<h2>Plain PHP File</h2>
 <ul>
     <li><a href="plain.php">page without extended template.</a></li>
-    <li><a href="sided.php">page with nested templates.</a></li>
-    <li><a href="sided2.php">page with nested templates but without starting section.</a></li>
+    <li><a href="plain2.php">page without extended template but without explicitly starting a section.</a></li>
 </ul>
--- public/plain2.php
+++ public/plain2.php
@@ -6,10 +6,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $page = PlainPages::self();
 ?>
-
----------------------------------
-contents start here.
----------------------------------
-
 <h1>This is a plain page</h1>
-<p>for raw php with low maintenance web sites. </p>
+<p>for raw php with low maintenance web sites but without explicitly starting a section. </p>
